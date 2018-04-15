d127e9812053714f7d943d2009a0dc00ecf5d746
Anuj <virtualanuj@gmail.com>
2018-04-15 19:27:15 +0530

    info function

---
+++ info/func.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	fdk "github.com/fnproject/fdk-go"
+)
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+}
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+	p := &Person{Name: "World!", Age: 10}
+	json.NewDecoder(in).Decode(p)
+	msg := struct {
+		Msg string `json:"message"`
+	}{
+		Msg: fmt.Sprintf("Hello %s %s", p.Name, p.Age),
+	}
+	json.NewEncoder(out).Encode(&msg)
+}
